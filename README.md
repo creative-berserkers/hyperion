1e3b8ed128fb659fefc9896d7625f828cbeb9c0f
Krzysztof Kabat <odrinwhite@hotmail.com>
2015-01-07 00:13:25 +0000

    version 1.0.20

--- README.md
+++ README.md
@@ -2,4 +2,25 @@ hyperion
 ========
 
 
-Custom web protocol implementation with Publish & Subscribe and Point to Point RPC
+Custom web protocol implementation with Publish & Subscribe and Point to Point RPC,
+
+here is an example of server:
+
+```js
+
+var hyperion = require('cb-hyperion').Server;
+
+var server = new hyperion(8080);
+var allBroadcast = server.registerBroadcast('all');
+
+server.registerMethod('join', function(ws){
+    allBroadcast.addTarget(ws);
+    console.log('joined all');
+});
+
+server.registerMethod('send', function(ws, msg){
+    allBroadcast.send(msg);
+    console.log('sending to all:'+msg);
+});
+
+```
--- hyperion.js
+++ hyperion.js
@@ -1,6 +1,81 @@
-var ws = require("ws");
+var WebSocketServer = require('ws').Server
 
+exports.Server = function bootstrap(port){
+    var self = this
+    this.methods = []
+    this.broadcasts = []
+    this.newConnections = null
+    this.wss = new WebSocketServer({port: port})
+  
+    this.wss.on('connection', function(ws) {
+        if(self.newConnections) self.newConnections.callback.call(self.newConnections.ctx, ws)
+        ws.on('message', function(message) {
+            if (message){
+                try {
+                    var msg = JSON.parse(message)
+                    if(!msg.hasOwnProperty('name')){
+                        console.log('missing property name')
+                        return 
+                    } 
+                    if(!msg.hasOwnProperty('arguments')){
+                        console.log('missing property arguments')
+                        return
+                    } 
+                    var method = self.methods[msg.name]
+                    if(method){ 
+                        method.callback.apply(method.ctx,[ws].concat(msg.arguments))
+                    } else {
+                        console.log('method '+msg.name+' not found')
+                    }
+                } catch(e){
+                    console.log('wrong message format: '+e)
+                }
+            }
+        })
+    })
+}
 
-exports = function bootstrap(port, appDir){
+exports.Server.prototype.registerNewConnection = function(callback, ctx){
+    this.newConnections = { 
+        callback: callback,
+        ctx : ctx
+    }
+}
+
+exports.Server.prototype.registerMethod = function(name, callback, ctx){
+    this.methods[name] = { 
+        callback: callback,
+        ctx : ctx
+    }
+}
+
+exports.Server.prototype.unregisterMethod = function(name){
+    this.methods[name] = null
+}
+
+exports.Server.prototype.registerBroadcast = function(name){
     
+    var targets = []
+    
+    var broadcast = {
+        addTarget : function(target){
+            targets.push(target)
+        },
+        send : function(msg){
+            targets.forEach(function(ws){
+                ws.send(JSON.stringify({
+                    type : 'broadcast',
+                    name : name,
+                    msg : msg
+                }))
+            })
+        }
+    }
+    
+    this.broadcasts[name] = broadcast
+    return broadcast
+}
+
+exports.Server.prototype.unregisterBroadcast = function(name){
+    this.broadcasts[name] = null
 }
